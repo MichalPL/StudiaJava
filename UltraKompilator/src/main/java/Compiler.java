e08ec6888440ded671a7eff858e5ddcec1846408
mpopek <218243@student.pwr.edu.pl>
2016-03-23 21:39:34 +0100

    CompilatorNewFunctions

--- UltraKompilator/src/main/java/Compiler.java
+++ UltraKompilator/src/main/java/Compiler.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Michal on 2016-03-17.
@@ -9,6 +11,7 @@ public class Compiler {
     FileWorker fileWorker;
     Converter converter;
     ArrayList<Mnemonic> list;
+    Map<String, Integer> labelMap = new HashMap<String, Integer>();
 
     public Compiler(MnemonicMap map, RegistryMap registryMap, FileWorker fileWorker, Converter converter)
     {
@@ -17,6 +20,7 @@ public class Compiler {
         this.fileWorker = fileWorker;
         this.converter = converter;
         list = new ArrayList<Mnemonic>();
+        labelMap = new HashMap<String, Integer>();
     }
 
     public void compile()
@@ -24,63 +28,74 @@ public class Compiler {
         if(fileWorker.getLinesCount() == -1) return;
         System.out.println("Trwa kompilacja...");
         Mnemonic mnemonic;
+        int lineCounter = 0;
         for (int i = 1; i <= fileWorker.getLinesCount(); i++) {
             String line = fileWorker.getLine(i).replaceAll("^\\s+", "");
             if(!line.isEmpty() && !line.startsWith(";")) {
-                String[] splitString = line.split("\\W+");
-                splitString[0] = splitString[0].toLowerCase();
-                if (isValidMnemonic(splitString[0])) { //poprawny mnemonik?
-                    if(map.getArgCount(splitString[0]) == -1) {
-                        System.out.println("Bład podczas kompilacji w linii: " + i);
-                        System.exit(0);
-                    } else if(map.getArgCount(splitString[0]) == 0) { //typu clear
-                        mnemonic = new Mnemonic(splitString[0], "a", "0");
-                        list.add(mnemonic);
-                    } else if(map.getArgCount(splitString[0]) == 1) {
-                        splitString[0] = splitString[0].toLowerCase();
-                        splitString[1] = splitString[1].toLowerCase();
-                        if (isValidArgWhenOnlyOne(splitString[0], splitString[1])) { //poprawny arg? typu jump
-                            mnemonic = new Mnemonic(splitString[0], "a", splitString[1]);
-                            list.add(mnemonic);
-                        } else {
-                            System.out.println("Blad! Nieprawidlowy argument 1 w linii: " + i);
+                lineCounter++;
+                if (isLabel(line.replaceAll("\\s", ""))) { //jeśli to etykieta
+                    labelMap.put(line.replaceAll("\\W+", ""), lineCounter + 1);
+                } else {
+                    String[] splitString = line.split("\\W+");
+                    splitString[0] = splitString[0].toLowerCase();
+                    if (isValidMnemonic(splitString[0]) || isLabel(splitString[0])) { //poprawny mnemonik?
+                        if (map.getArgCount(splitString[0]) == -1) {
+                            System.out.println("Bład podczas kompilacji w linii: " + i);
                             System.exit(0);
-                        }
-                    } else if(map.getArgCount(splitString[0]) == 2) {
-                        splitString[0] = splitString[0].toLowerCase();
-                        splitString[1] = splitString[1].toLowerCase();
-                        splitString[2] = splitString[2].toLowerCase();
-                        if (isValidArg1(splitString[1])) { //poprawny arg1?
-                            if (isValidArg2(splitString[0], splitString[2])) { //poprawny arg2?
-                                if (!isTheSameRegisty(splitString[0], splitString[1], splitString[2])) {
-                                    if(isSyntaxCorrect(line)) {
-                                        mnemonic = new Mnemonic(splitString[0], splitString[1], splitString[2]);
-                                        list.add(mnemonic);
+                        } else if (map.getArgCount(splitString[0]) == 0) { //typu clear
+                            //jeśli mnemonik bez argumentowy
+                            mnemonic = new Mnemonic(splitString[0], "a", "0");
+                            list.add(mnemonic);
+                        } else if (map.getArgCount(splitString[0]) == 1) {
+                            splitString[0] = splitString[0].toLowerCase();
+                            splitString[1] = splitString[1].toLowerCase();
+                            if (isValidArgWhenOnlyOne(splitString[0], splitString[1])) { //jesli poprawny argument mnemonika (nie uwzgledniajac jmp)
+                                mnemonic = new Mnemonic(splitString[0], "a", splitString[1]);
+                                list.add(mnemonic);
+                            } else if (isJump(splitString[0], splitString[1])) { //jesli to jump
+                                mnemonic = new Mnemonic(splitString[0], "a", splitString[1]);
+                                list.add(mnemonic);
+                            } else {
+                                System.out.println("Blad! Nieprawidlowy argument 1 w linii: " + i);
+                                System.exit(0);
+                            }
+                        } else if (map.getArgCount(splitString[0]) == 2) {
+                            splitString[0] = splitString[0].toLowerCase();
+                            splitString[1] = splitString[1].toLowerCase();
+                            splitString[2] = splitString[2].toLowerCase();
+                            if (isValidArg1(splitString[1])) { //poprawny arg1?
+                                if (isValidArg2(splitString[0], splitString[2])) { //poprawny arg2?
+                                    if (!isTheSameRegisty(splitString[0], splitString[1], splitString[2])) { // jesli nie jest ten sam rejestr
+                                        if (isSyntaxCorrect(line)) { //poprawna skladnia (przecinek miedzy argumentami)
+                                            mnemonic = new Mnemonic(splitString[0], splitString[1], splitString[2]);
+                                            list.add(mnemonic);
+                                        } else {
+                                            System.out.println("Blad! Niepoprawna skladnia! Sprawdz przecinek w linii " + i);
+                                            System.exit(0);
+                                        }
                                     } else {
-                                        System.out.println("Blad! Niepoprawna skladnia! Sprawdz przecinek w linii " + i);
+                                        System.out.println("Blad! Probujesz wykonac operacje na tym samym rejestrze w linii: " + i);
                                         System.exit(0);
                                     }
                                 } else {
-                                    System.out.println("Blad! Probujesz wykonac operacje na tym samym rejestrze w linii: " + i);
+                                    System.out.println("Blad! Nieprawidlowy argument 2 w linii: " + i);
+                                    System.out.println(splitString[2]);
                                     System.exit(0);
                                 }
                             } else {
-                                System.out.println("Blad! Nieprawidlowy argument 2 w linii: " + i);
-                                System.out.println(splitString[2]);
+                                System.out.println("Blad! Nieprawidlowy argument 1 w linii: " + i);
                                 System.exit(0);
                             }
                         } else {
-                            System.out.println("Blad! Nieprawidlowy argument 1 w linii: " + i);
+                            System.out.println("Bład kompilacji w linii: " + i);
                             System.exit(0);
                         }
+
                     } else {
-                        System.out.println("Bład kompilacji w linii: " + i);
+                        System.out.println("Blad! Nieprawidlowy mnemonik w linii: " + i);
+                        System.out.println(splitString[0]);
                         System.exit(0);
                     }
-
-                } else {
-                    System.out.println("Blad! Nieprawidlowy mnemonik w linii: " + i);
-                    System.exit(0);
                 }
             }
         }
@@ -94,15 +109,21 @@ public class Compiler {
         fileWorker.createFile();
         for (Mnemonic aList : list) {
             fileWorker.addToFile(map.getBinary(aList.getName()));
-            //System.out.println(map.getBinary(aList.getName()));
             fileWorker.addToFile(registryMap.getBinary(aList.getArg1()));
-            //System.out.println(registryMap.getBinary(aList.getArg1()));
-            if (aList.getName().endsWith("x")) { //jesli rejestr z rejestrem
-                fileWorker.addToFile("00000" + registryMap.getBinary(aList.getArg2()));
-                //System.out.println("00000" + registryMap.getBinary(aList.getArg2()));
-            } else if (isNumber(aList.getArg2()) && !aList.getName().endsWith("x")) { //jesli rejestr z liczba
-                fileWorker.addToFile(converter.getString(converter.convertToU2(aList.getArg2())));
-                //System.out.println(converter.getString(converter.convertToU2(aList.getArg2())));
+            if (aList.getName().endsWith("x") || aList.getName().equals("inc") || aList.getName().equals("dec")) {
+                fileWorker.addToFile("00000" + registryMap.getBinary(aList.getArg2()) + "\n");
+            } else if (!aList.getName().endsWith("x") && isNumber(aList.getArg2())) { //jesli rejestr z liczba
+                fileWorker.addToFile(converter.getString(converter.convertToU2(aList.getArg2())) + "\n");
+            } else if(isJump(aList.getName(), aList.getArg2())) {
+                if(labelMap.get(aList.getArg2()) != null) {
+                    fileWorker.addToFile(converter.getString(converter.convertToU2(labelMap.get(aList.getArg2()).toString())));
+                } else {
+                    System.out.println("Blad! Sprawdz etykiety!");
+                    System.exit(0);
+                }
+            } else {
+                System.out.println("Blad podczas przetwarzania!");
+                System.exit(0);
             }
         }
     }
@@ -130,8 +151,13 @@ public class Compiler {
         if((name.equals("negx") || name.equals("inc")
                 || name.equals("dec")) && registryMap.isRegistry(arg)) return true;
         else if((name.equals("neg") || name.equals("movzx")) && isNumber(arg)) return true;
-        else if(name.startsWith("j")) return true; //TODO: jump nie jest zrobiony!
         return false;
+    }
+
+    private boolean isJump(String name, String arg)
+    {
+        return (name.equals("jmp")) //|| name.equals("jw") || name.equals("jm") || name.equals("jr")
+                && !isNumber(arg);
     }
 
     private boolean isTheSameRegisty(String name, String arg1, String arg2) //zeby uniknac np. kopiowania z tego samego rejestru
@@ -157,5 +183,10 @@ public class Compiler {
     {
         String[] splitString = line.split("\\W+");
         return (map.getArgCount(splitString[0].toLowerCase()) == 2 && line.contains(","));
+    }
+
+    private boolean isLabel(String name)
+    {
+        return name.endsWith(":");
     }
 }
